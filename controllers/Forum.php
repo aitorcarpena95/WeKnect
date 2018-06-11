645a24a8d211398c5f7d0e6b2912684245635171
aitorcarpena95 <32368842+aitorcarpena95@users.noreply.github.com>
2018-06-11 14:25:14 +0200

    Update Forum.php

--- controllers/Forum.php
+++ controllers/Forum.php
@@ -35,12 +35,12 @@ class Forum extends CI_Controller {
 	 */
 	public function index($slug = false) {
 		
-		// create the data object
+		// create los objetos de data
 		$data = new stdClass();
 		
 		if ($slug === false) {
 			
-			// create objects
+			// crea objetos
 			$forums = $this->forum_model->get_forums();
 			
 			foreach ($forums as $forum) {
@@ -52,14 +52,14 @@ class Forum extends CI_Controller {
 				
 				if ($forum->count_topics > 0) {
 					
-					// $forum has topics
+					// $forum tiene los subforos
 					$forum->latest_topic            = $this->forum_model->get_forum_latest_topic($forum->id);
 					$forum->latest_topic->permalink = $forum->slug . '/' . $forum->latest_topic->slug;
 					$forum->latest_topic->author    = $this->user_model->get_username_from_user_id($forum->latest_topic->user_id);
 					
 				} else {
 					
-					// $forum doesn't have topics yet
+					// $forum no tiene subforos aún
 					$forum->latest_topic = new stdClass();
 					$forum->latest_topic->permalink = null;
 					$forum->latest_topic->title = null;
@@ -70,30 +70,30 @@ class Forum extends CI_Controller {
 	
 			}
 			
-			// create breadcrumb
+			// crear breadcrumb
 			$breadcrumb  = '<ol class="breadcrumb">';
 			$breadcrumb .= '<li class="active" style="color:black">Home</li>';
 			$breadcrumb .= '</ol>';
 			
-			// assign created objects to the data object
+			// assignar objectos creados a los data object
 			$data->forums     = $forums;
 			$data->breadcrumb = $breadcrumb;
 			
-			// load views and send data
+			// carga las vistas y envia data
 			$this->load->view('header');
 			$this->load->view('forum/index', $data);
 			$this->load->view('footer');
 			
 		} else {
 			
-			// get id from slug
+			// sacar id de slug
 			$forum_id = $this->forum_model->get_forum_id_from_forum_slug($slug);
 			
-			// create objects
+			// crear objetos
 			$forum    = $this->forum_model->get_forum($forum_id);
 			$topics   = $this->forum_model->get_forum_topics($forum_id);
 			
-			// create breadcrumb
+			// crear breadcrumb
 			$breadcrumb  = '<ol class="breadcrumb">';
 			$breadcrumb .= '<li><a href="' . base_url() . '">Home</a></li>';
 			$breadcrumb .= '<li class="active" style="color:black">' . $forum->title . '</li>';
@@ -110,12 +110,12 @@ class Forum extends CI_Controller {
 				
 			}
 			
-			// assign created objects to the data object
+			// assignar objetos creados a los data object
 			$data->forum      = $forum;
 			$data->topics     = $topics;
 			$data->breadcrumb = $breadcrumb;
 			
-			// load views and send data
+			// carga vistas y envia data
 			$this->load->view('header');
 			$this->load->view('forum/single', $data);
 			$this->load->view('footer');
@@ -132,60 +132,60 @@ class Forum extends CI_Controller {
 	 */
 	public function create_forum() {
 		
-		// create the data object
+		// crea objetos data
 		$data = new stdClass();
 		
-		// if the user is not logged in as administrator, he cannot create a new forum
+		// Si el usuario no esta logueado como admin, no puede crear un nuevo foro
 		if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
 			$data->login_as_admin_needed = true;
 		} else {
 			$data->login_as_admin_needed = false;
 		}
 		
-		// create breadcrumb
+		// crear breadcrumb
 		$breadcrumb  = '<ol class="breadcrumb">';
 		$breadcrumb .= '<li><a href="' . base_url() . '">Home</a></li>';
 		$breadcrumb .= '<li class="active" style="color:black">Crear un nuevo foro</li>';
 		$breadcrumb .= '</ol>';
 		
-		// assign breadcrumb to the data object
+		// asignar breadcrumb a el objeto data
 		$data->breadcrumb = $breadcrumb;
 		
-		// load form helper and validation library
+		// cargar el helper y la libreria de validacion
 		$this->load->helper('form');
 		$this->load->library('form_validation');
 		
-		// set validation rules
+		// especificar las reglas
 		$this->form_validation->set_rules('title', 'Forum Title', 'trim|required|min_length[4]|max_length[255]|is_unique[forums.title]', array('is_unique' => 'El titulo que has propuesto ya existe, piensa otro!'));
 		$this->form_validation->set_rules('description', 'Description', 'trim|max_length[80]');
 		
 		if ($this->form_validation->run() === false) {
 			
-			// keep what the user has entered previously on fields
+			// mantener los datos que el usuario habia puesto en los campos
 			$data->title       = $this->input->post('title');
 			$data->description = $this->input->post('description');
 			
-			// validation not ok, send validation errors to the view
+			// la validacion no es correcta, envia errores a la vista
 			$this->load->view('header');
 			$this->load->view('forum/create/create', $data);
 			$this->load->view('footer');
 			
 		} else {
 			
-			// set variables from the form
+			// concreta variables del formulario
 			$title       = $this->input->post('title');
 			$description = $this->input->post('description');
 			
 			if ($this->forum_model->create_forum($title, $description)) {
 				
-				// forum creation ok
+				// creacion del foro ok
 				$this->load->view('header');
 				$this->load->view('forum/create/create_success', $data);
 				$this->load->view('footer');
 				
 			} else {
 				
-				// forum creation failed, this should never happen
+				// creacion del foro fallida, esto no deberia pasar
 				$data->error = 'Wops! Ocurrió un problema creando el foro, intentalo de nuevo.';
 				
 				// send error to the view
@@ -211,65 +211,65 @@ class Forum extends CI_Controller {
 		// create the data object
 		$data = new stdClass();
 		
-		// if the user is not logged in, he cannot create a new topic
+		// Si el usuario no esta logueado, no puede crear subforos
 		if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 			$data->login_needed = true;
 		} else {
 			$data->login_needed = false;
 		}
 		
-		// set variables from the the URI
+		// especificar variables de la URI
 		$forum_slug = $this->uri->segment(1);
 		$forum_id   = $this->forum_model->get_forum_id_from_forum_slug($forum_slug);
 		$forum      = $this->forum_model->get_forum($forum_id);
 		
-		// create breadcrumb
+		// crear breadcrumb
 		$breadcrumb  = '<ol class="breadcrumb">';
 		$breadcrumb .= '<li><a href="' . base_url() . '">Home</a></li>';
 		$breadcrumb .= '<li style="color:black"><a href="' . base_url("index.php/".$forum->slug) . '">' . $forum->title . '</a></li>';
 		$breadcrumb .= '<li class="active" style="color:black">Crear un nuevo subforo</li>';
 		$breadcrumb .= '</ol>';
 		
-		// assign breadcrumb to the data object
+		// asignar breadcrumb al objeto data
 		$data->breadcrumb = $breadcrumb;
 		
-		// load form helper and validation library
+		// cargar helper y libreria de validacion
 		$this->load->helper('form');
 		$this->load->library('form_validation');
 		
-		// set validation rules
+		// especificar reglas
 		$this->form_validation->set_rules('title', 'Topic Title', 'trim|required|min_length[4]|max_length[255]|is_unique[topics.title]', array('is_unique' => 'El subforo que has intentado crear ya existe, intentalo de nuevo cambiando algo!'));
 		$this->form_validation->set_rules('content', 'Content', 'required|min_length[4]');
 		
 		if ($this->form_validation->run() === false) {
 			
-			// keep what the user has entered previously on fields
+			// mantener los datos que el usuario habia puesto en los campos
 			$data->title   = $this->input->post('title');
 			$data->content = $this->input->post('content');
 			
-			// validation not ok, send validation errors to the view
+			// validacion no ok, enviar errores de validación a la vista
 			$this->load->view('header');
 			$this->load->view('topic/create/create', $data);
 			$this->load->view('footer');
 			
 		} else {
 			
-			// set variables from the form
+			// especificar variables del formulario
 			$title   = $this->input->post('title');
 			$content = $this->input->post('content');
 			$user_id = $_SESSION['user_id'];
 			
 			if ($this->forum_model->create_topic($forum_id, $title, $content, $user_id)) {
 				
-				// topic creation ok
+				// creación del subforo ok
 				redirect("https://knect.asir6.enganxat.net/index.php/".$forum_slug . '/' . strtolower(url_title($title)));
 				
 			} else {
 				
-				// topic creation failed, this should never happen
+				// error al crear el subforo, esto no debería pasar
 				$data->error = 'Wops! Hubo un problema creando el subforo, intentalo de nuevo.';
 				
-				// send error to the view
+				// envia errores a la vista
 				$this->load->view('header');
 				$this->load->view('topic/create/create', $data);
 				$this->load->view('footer');
@@ -290,14 +290,14 @@ class Forum extends CI_Controller {
 	 */
 	public function topic($forum_slug, $topic_slug) {
 		
-		// create the data object
+		// crear objetos data
 		$data = new stdClass();
 		
-		// get ids from slugs
+		// coger ids de slugs
 		$forum_id = $this->forum_model->get_forum_id_from_forum_slug($forum_slug);
 		$topic_id = $this->forum_model->get_topic_id_from_topic_slug($topic_slug);
 		
-		// create objects
+		// crear objetos
 		$forum = $this->forum_model->get_forum($forum_id);
 		$topic = $this->forum_model->get_topic($topic_id);
 		$posts = $this->forum_model->get_posts($topic_id);
@@ -308,20 +308,20 @@ class Forum extends CI_Controller {
 			
 		}
 		
-		// create breadcrumb
+		// crear breadcrumb
 		$breadcrumb  = '<ol class="breadcrumb">';
 		$breadcrumb .= '<li style="color:black"><a href="' . base_url() . '">Home</a></li>';
 		$breadcrumb .= '<li style="color:black"><a href="' . base_url("index.php/".$forum->slug) . '">' . $forum->title . '</a></li>';
 		$breadcrumb .= '<li class="active" style="color:black">' . $topic->title . '</li>';
 		$breadcrumb .= '</ol>';
 		
-		// assign created objects to the data object
+		// asignar obejos a los objetos data
 		$data->forum      = $forum;
 		$data->topic      = $topic;
 		$data->posts      = $posts;
 		$data->breadcrumb = $breadcrumb;
 		
-		// load views and send data
+		// cargar vistas y enviar data
 		$this->load->view('header');
 		$this->load->view('topic/single', $data);
 		$this->load->view('footer');
@@ -338,21 +338,21 @@ class Forum extends CI_Controller {
 	 */
 	public function create_post($forum_slug, $topic_slug) {
 		
-		// create the data object
+		// crear objeto data
 		$data = new stdClass();
 		
-		// if the user is not logged in, he cannot reply to a topic
+		// si el usuario no esta logueado, no puede postear una respuesta
 		if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 			$data->login_needed = true;
 		} else {
 			$data->login_needed = false;
 		}
 		
-		// get ids from slugs
+		// coger ids de slugs
 		$forum_id = $this->forum_model->get_forum_id_from_forum_slug($forum_slug);
 		$topic_id = $this->forum_model->get_topic_id_from_topic_slug($topic_slug);
 		
-		// create objects
+		// crear objetos
 		$forum = $this->forum_model->get_forum($forum_id);
 		$topic = $this->forum_model->get_topic($topic_id);
 		$posts = $this->forum_model->get_posts($topic_id);
@@ -376,19 +376,19 @@ class Forum extends CI_Controller {
 		$data->posts      = $posts;
 		$data->breadcrumb = $breadcrumb;
 		
-		// load form helper and validation library
+		// cargar el helper y la libreria de validación
 		$this->load->helper('form');
 		$this->load->library('form_validation');
 		
-		// set validation rules
+		// escpecificar las reglas
 		$this->form_validation->set_rules('reply', 'Reply', 'required|min_length[2]');
 		
 		if ($this->form_validation->run() === false) {
 			
-			// keep what the user has entered previously on fields
+			// mantener los datos que el usuario habia puesto en los campos
 			$data->content = $this->input->post('reply');
 			
-			// validation not ok, send validation errors to the view
+			// validación no ok, envia erores de validación a la vista
 			$this->load->view('header');
 			$this->load->view('topic/reply', $data);
 			$this->load->view('footer');
@@ -400,13 +400,13 @@ class Forum extends CI_Controller {
 			
 			if ($this->forum_model->create_post($topic_id, $user_id, $content)) {
 				
-				// post creation ok
+				// creación del post ok
 				redirect("https://knect.asir6.enganxat.net/index.php/".$forum_slug . '/' . $topic_slug);
                                 
 				
 			} else {
 				
-				// post creation failed, this should never happen
+				// creación del post fallida
 				$data->error = 'Wops! Hubo un problema con tu respuesta, intentalo de nuevo.';
 				
 				// send error to the view
